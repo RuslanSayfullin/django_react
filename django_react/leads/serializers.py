7b0f1719fdbfca26764dff951f68349b82909f31
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-11-03 09:50:17 +0500

    Создали новый проект под React

--- django_react/leads/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Lead
-
-
-class LeadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lead
-        fields = ('id', 'name', 'email', 'message')
--- django_react/leads/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/lead/', views.LeadListCreate.as_view()),
-]
