7b3ac4761a774113b1034cd5265fb1c1b03d1f1b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-03-20 12:45:31 +0500

    Creating the Home page

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ social-media-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>
+                Welcome!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
